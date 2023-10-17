bcccc1b2f6ef21afb95c7323359744b2bf888cca
WAYNESON CLYV SANTOS <waynesonclyv.santos.21@usjr.edu.ph>
2023-10-17 16:45:10 +0800

    Worked on employeelist

--- ViewModel/Dashboard_EmployeeDetails_ViewModel.cs
+++ ViewModel/Dashboard_EmployeeDetails_ViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Project_C.F_.Model;
+using Project_C.F_.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,28 @@ namespace Project_C.F_.ViewModel
 {
     public partial class Dashboard_EmployeeDetails_ViewModel : Dashboard_ViewModel
     {
+        public Dashboard_EmployeeDetails_ViewModel() 
+        {
+            employee_Services = new Employee_Services();
+            GetEmployee();
+        }
+        private readonly Employee_Services employee_Services;
 
+        private ObservableCollection<Employee> employeeList;
+        public ObservableCollection<Employee> EmployeeList
+        {
+            get { return employeeList; }
+            set { employeeList = value; OnPropertyChanged(); OnPropertyChanged(nameof(employeeList)); }
+        }
+        private void GetEmployee()
+        {
+            foreach(var person in employee_Services.GetEmployees()) 
+            {
+                if(person.JobPosition == "Employee")
+                {
+                    employeeList.Add(person);
+                }
+            }
+        }
     }
 }
