8e516b670e4fef1660b253f34c9d2d079730239f
WAYNESON CLYV SANTOS <waynesonclyv.santos.21@usjr.edu.ph>
2023-11-05 19:55:37 +0800

    Added Employee_Dashboard_EmployeeDetails from Hans Vincent and its corresponding viewmodel

--- ViewModel/Employee_Dashboard_EmployeeDetails_ViewModel.cs
+++ ViewModel/Employee_Dashboard_EmployeeDetails_ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_C.F_.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,31 @@ using System.Threading.Tasks;
 
 namespace Project_C.F_.ViewModel
 {
-    public partial class Employee_Dashboard_EmployeeDetails_ViewModel : MainViewModel
+    public partial class Employee_Dashboard_EmployeeDetails_ViewModel : Dashboard_ViewModel
     {
+        private Employee highlightedEmployeee;
+        public Employee HighlightedEmployeee
+        {
+            get { return highlightedEmployeee; }
+            set { highlightedEmployeee = value; OnPropertyChanged(); OnPropertyChanged(nameof(highlightedEmployeee)); SetDateDetails(); }
+        }
+        private void SetDateDetails()
+        {
+            HighlightEmployeeBirthday = CurrentEmployee.BirthDate.ToString("MM/dd/yyyy");
+            HighlightEmployeeDateJoined = CurrentEmployee.DateJoined.ToString("MM/dd/yyyy");
+        }
 
+        private string highlightEmployeeBirthday;
+        public string HighlightEmployeeBirthday
+        {
+            get { return highlightEmployeeBirthday; }
+            set { highlightEmployeeBirthday = value; OnPropertyChanged(); OnPropertyChanged(nameof(highlightEmployeeBirthday)); }
+        }
+        private string highlightEmployeeDateJoined;
+        public string HighlightEmployeeDateJoined
+        {
+            get { return highlightEmployeeDateJoined; }
+            set { highlightEmployeeDateJoined = value; OnPropertyChanged(); OnPropertyChanged(nameof(highlightEmployeeDateJoined)); }
+        }
     }
 }
